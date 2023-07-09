92b84ca82952f2f6cddfda9cd910c4eee3b4d8b4
Matěj grohmann <grohmannmatej99@gmail.com>
2023-07-09 22:20:33 +0200

    initial commit - project structure setup
    empty flutter project

---
+++ android/app/src/main/kotlin/com/scoor/note_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.scoor.note_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/application/note_app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    const NoteApp(),
+  );
+}
