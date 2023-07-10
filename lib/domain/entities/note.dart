a57b134b9c3d2174de43e8e3dacfc3b3c92c1dd5
Matěj grohmann <grohmannmatej99@gmail.com>
2023-07-10 13:13:18 +0200

    initial note

--- lib/domain/entities/note.dart
+++ lib/domain/entities/note.dart
@@ -5,11 +5,21 @@ import 'value_objects/note_time_stamp.dart';
 import 'value_objects/note_title.dart';
 
 class Note extends Equatable {
-  final int id;
+  final String id;
   final NoteDescription description;
   final NoteTitle title;
   final NoteTimeStamp timeStamp;
   final NoteStatus status;
+
+  factory Note.create({required String id}) {
+    return Note._(
+      id: id,
+      status: NoteStatus.initial(),
+      description: NoteDescription.initial(),
+      title: NoteTitle.initial(),
+      timeStamp: NoteTimeStamp.initial(),
+    );
+  }
 
   const Note._({
     required this.id,
